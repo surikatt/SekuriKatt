763e8e6b82e9a5b7039bb6c52810b3db18b53dae
alexis <sawcce@proton.me>
2023-03-12 15:35:32 +0100

    Interface basique + streaming video

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_vlc_player/flutter_vlc_player.dart';
+
+import './components/app_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,20 +19,44 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark(
         useMaterial3: true,
       ),
-      home: const App(),
+      home: App(),
     );
   }
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  App({super.key});
+
+  final VlcPlayerController _vlcViewController = VlcPlayerController.network(
+      "test",
+      autoPlay: true,
+      options: VlcPlayerOptions(
+          video: VlcVideoOptions([VlcVideoOptions.dropLateFrames(true)]),
+          http: VlcHttpOptions([VlcHttpOptions.httpReconnect(true)])));
 
   @override
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.light,
         child: SizedBox.expand(
-          child: Container(color: Theme.of(context).colorScheme.background),
+          child: Container(
+            color: Theme.of(context).colorScheme.background,
+            child: SafeArea(
+              child: Column(
+                children: [
+                  const ExtendedAppBar(),
+                  VlcPlayer(
+                    controller: _vlcViewController,
+                    aspectRatio: 16 / 9,
+                    placeholder: Text(
+                      "Loading",
+                      style: Theme.of(context).typography.dense.bodyLarge,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ));
   }
 }
