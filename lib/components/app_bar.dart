0a13b559cb7539391dd59a37424bf7067e7b6304
alexis <sawcce@proton.me>
2023-03-12 15:34:52 +0100

    Test barre d'appli

---
+++ lib/components/app_bar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ExtendedAppBar extends StatelessWidget {
+  const ExtendedAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      height: 250,
+      color: Theme.of(context).colorScheme.background,
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "SekuriKatt",
+              style: Theme.of(context).typography.tall.headlineLarge,
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [Icon(Icons.camera), Icon(Icons.door_back_door)],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
